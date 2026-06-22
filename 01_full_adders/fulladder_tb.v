5ae646dfa48e7194e1f298b8ab30e53dc6669c8d
Anbarasu G <anbarasug302@gmail.com>
2026-06-22 23:25:02 +0530

    Added Full Adder Verilog testbench
    Implemented a testbench for the full adder module, including stimulus and display of results for all input combinations.

---
+++ 01_full_adders/fulladder_tb.v
@@ -0,0 +1,68 @@
+// Code your testbench here
+// or browse Examples
+module full_adder_tb;
+  reg A;
+  reg B;
+  reg C;
+  wire Sum;
+  wire Carry;
+  
+  full_adder dut (
+    .A(A),
+    .B(B),
+    .C(C),
+    .Sum(Sum),
+    .Carry(Carry));
+  
+  initial begin
+    $dumpfile("full_adder");
+    $dumpvars(0,full_adder_tb);
+    
+    
+    
+               A=0;B=0;C=0; #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+    
+    
+    
+               A=0;B=0;C=1; #10;
+               
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+  
+    
+    
+              A=0;B=1;C=0;  #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+  
+  
+    
+                A=0;B=1;C=1;  #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+  
+    
+    
+                A=1;B=0;C=0;  #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+  
+    
+    
+                A=1;B=0;C=1; #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+    
+    
+                A=1;B=1;C=0;  #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+  
+  
+   
+    
+                A=1;B=1;C=1;  #10;
+    $display("A=%b------B=%b-----C=%b-----Sum=%b----Carry=%b",A,B,C,Sum,Carry);
+    
+    #20;
+    $finish;
+  end
+endmodule
+  
+  
+  
