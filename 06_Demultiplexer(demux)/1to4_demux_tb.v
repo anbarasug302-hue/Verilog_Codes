c76884fd691b71517ed3af7eb4a6ee8897a42b4f
Anbarasu G <anbarasug302@gmail.com>
2026-06-29 12:25:04 +0530

    Create testbench for 1-to-4 demultiplexer verilog using ternary operator
    Added a testbench for the 1-to-4 demultiplexer, including signal initialization and display outputs.

---
+++ 06_Demultiplexer(demux)/1to4_demux_tb.v
@@ -0,0 +1,42 @@
+// Code your testbench here
+// or browse Examples
+module demux_1to4_tb ;
+  reg a;
+  reg s1;
+  reg s0;
+  wire y0;
+  wire y1;
+  wire y2;
+  wire y3;
+  
+  
+  demux_1to4 dut (.a(a),
+                  .s1(s1),
+                  .s0(s0),
+                  .y0(y0),
+                  .y1(y1),
+                  .y2(y2),
+                  .y3(y3));
+  
+  
+  initial 
+    begin
+      
+      $dumpfile("demux_1to4.vcd");
+      $dumpvars(0,demux_1to4_tb);
+      
+      a=1; s1=0; s0=0; #10;
+      $display("a=%b-----s1=%b----s0=%b----y0=%b-----y1=%b----y2=%b----y3=%b" ,a,s1,s0,y0,y1,y2,y3);
+      
+        a=1; s1=0; s0=1; #10;
+      $display("a=%b-----s1=%b----s0=%b----y0=%b-----y1=%b----y2=%b----y3=%b" ,a,s1,s0,y0,y1,y2,y3);
+      
+        a=1; s1=1; s0=0; #10;
+      $display("a=%b-----s1=%b----s0=%b----y0=%b-----y1=%b----y2=%b----y3=%b" ,a,s1,s0,y0,y1,y2,y3);
+      
+        a=1; s1=1; s0=1; #10;
+      $display("a=%b-----s1=%b----s0=%b----y0=%b-----y1=%b----y2=%b----y3=%b" ,a,s1,s0,y0,y1,y2,y3);
+      
+      $finish;
+    end
+endmodule
