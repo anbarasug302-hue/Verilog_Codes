f55b7305095682b3018ed8804024782db7eb2fe9
Anbarasu G <anbarasug302@gmail.com>
2026-06-30 14:49:45 +0530

    Implement 3-to-8 decoder in Verilog in boolen operation

---
+++ 07_decoder/decoder_3to8_designs.v
@@ -0,0 +1,25 @@
+// Code your design here
+module decoder_3to8 (a,b,c,y0,y1,y2,y3,y4,y5,y6,y7);
+  input a;
+  input b;
+  input c;
+  output y0;
+  output y1;
+  output y2;
+  output y3;
+  output y4;
+  output y5;
+  output y6;
+  output y7;
+  
+  assign y0 = ~ a & ~ b & ~ c;
+  assign y1 = ~ a & ~ b &   c; 
+  assign y2 = ~ a &   b & ~ c;
+  assign y3 = ~ a &   b &   c;
+  assign y4 =   a & ~ b & ~ c;
+  assign y5 =   a & ~ b &   c;
+  assign y6 =   a &   b & ~ c;
+  assign y7 =   a &   b &   c;
+    
+  
+endmodule
