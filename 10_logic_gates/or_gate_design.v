6a974c3ac8f682e82cbd2badb7eab53e38b0dbc5
Anbarasu G <anbarasug302@gmail.com>
2026-07-03 15:13:35 +0530

    Implement OR gate design in Verilog . both boolean and ternary operator

---
+++ 10_logic_gates/or_gate_design.v
@@ -0,0 +1,24 @@
+// Code your design here
+module or_gate (a,b,y);
+  input a;
+  input b;
+  output y;
+  
+  // boolean expression 
+  
+  
+  
+  assign y = a | b ;
+ 
+  
+  
+  
+  // ternary operator 
+  
+  /*
+  
+  assign y =  ( a == 0 &&  b == 0 ) ? 1'b0 : 1'b1 ;
+  
+  */
+  
+endmodule
